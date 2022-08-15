a7d06afc259f9ad99aee8b0a5a00a2019ca6a7df
Truc Phan <trucphan@my.unt.edu>
2022-08-15 18:42:12 -0500

    set up

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hellooo');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHVCIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vY29mZmVlc2hvcC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdIZWxsb29vJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
